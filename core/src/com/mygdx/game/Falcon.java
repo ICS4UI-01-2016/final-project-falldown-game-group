0fc7f6ebb39dce0b36bb06c3270603c2d8245389
fostp4040 <fostp4040@HRH-IC0027327.wrdsb.ca>
2016-12-22 14:18:47 -0500

    added Falcon class

---
+++ core/src/com/mygdx/game/Falcon.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ *
+ * @author lamon
+ */
+public class Falcon {
+    private Vector3 position;
+    private Vector3 velocity;
+    private Texture falconPic;
+    private Rectangle bounds;
+    
+    private final float GRAVITY = -15;
+    private final float MOVEMENT = 100;
+    
+    public Falcon(int x, int y){
+        position = new Vector3(x,y,0);
+        velocity = new Vector3(MOVEMENT,0,0);
+        falconPic = new Texture("bird.png");
+        bounds = new Rectangle(position.x, position.y, falconPic.getWidth(), falconPic.getHeight());
+    }
+    
+    public void jump(){
+        velocity.y = 250;
+    }
+    
+    public void update(float deltaTime){
+        // add gravity
+        velocity.y += GRAVITY;
+        // scaling velocity by time
+        velocity.scl(deltaTime);
+        // adding velocity to position
+        position.add(velocity);
+        // unscale velocity
+        velocity.scl(1/deltaTime);
+        
+        // set the new bounds
+        bounds.setPosition(position.x, position.y);
+    }
+    
+    public void render(SpriteBatch batch){
+        batch.draw(falconPic, position.x, position.y);
+    }
+    
+    public float getX(){
+        return position.x;
+    }
+    
+    public float getY(){
+        return position.y;
+    }
+    
+    public Rectangle getBounds(){
+        return bounds;
+    }
+    
+    public void dispose(){
+        falconPic.dispose();
+    }
+}
