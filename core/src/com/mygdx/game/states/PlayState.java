34d204630668e23e677a70e1cd07dd89c9619a9a
fostp4040 <fostp4040@HRH-IC0027327.wrdsb.ca>
2017-01-19 13:11:44 -0500

    added collision to Falcon and PlayState Class

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -61,9 +61,9 @@ public class PlayState extends State {
         // draw the background
         batch.draw(bg, getCameraX() - getViewWidth() / 2, getCameraY() - getViewHeight() / 2);
         font.draw(batch, ""+score, getCameraX(), getCameraY()+150);
-        // draw the bird
+        // draw the Falcon
         falcon.render(batch);
-        // draw pipes
+        // draw obstacles
         for (int i = 0; i < obstacles.length; i++) {
             obstacles[i].render(batch);
         }
@@ -78,7 +78,7 @@ public class PlayState extends State {
         // move the camera to match the bird
         moveCameraY(falcon.getY() + CAM_Y_OFFSET);
 
-        // did Falcon hit the top of the screen
+        // did the Falcon hit the top of the screen
         if (falcon.getY() >= MyGdxGame.WIDTH) {
             // end the game
             StateManager gsm = getStateManager();
@@ -95,16 +95,7 @@ public class PlayState extends State {
         // did the Falcon hit a obstacle
         for (int i = 0; i < obstacles.length; i++) {
             if (obstacles[i].collides(falcon)) {
-                // end the game
-                StateManager gsm = getStateManager();
-                // pop off the game screen to go to menu
-                gsm.pop();
-                Preferences pref=Gdx.app.getPreferences("highScore");
-                int highScore=pref.getInteger("highScore",0);
-                if(score>highScore){
-                    pref.putInteger("highScore", score);
-                    pref.flush();
-                }
+                falcon.collide();
             }else if(!obstacles[i].hasPassed()&&falcon.getX()>obstacles[i].getX()+Obstacle.WIDTH){
                 score++;
                 obstacles[i].pass();
@@ -113,7 +104,7 @@ public class PlayState extends State {
 
         // adjust the pipes
         for (int i = 0; i < obstacles.length; i++) {
-            // has the bird passed the pipe
+            // has the falcon passed the obstacle
             if (getCameraX() - MyGdxGame.WIDTH / 4 > obstacles[i].getX() + Obstacle.WIDTH) {
                 float x = obstacles[i].getX() + OBSTACLE_GAP_AMOUNT * Obstacle.WIDTH * obstacles.length;
                 obstacles[i].setX(x);
